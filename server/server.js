461f03652a783f92df548de924e432d1a66c9763
Ryugi62 <xorjf1027@naver.com>
2022-12-08 18:36:04 +0900

    server : node 서버 추가

---
+++ server/server.js
@@ -0,0 +1,10 @@
+// express server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// serve static files
+app.use(express.static("public"));
+
+// start server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
