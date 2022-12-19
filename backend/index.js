833e44c78b03944dce20ab479a3659a1bef12886
Ryugi62 <xorjf1027@naver.com>
2022-12-19 12:57:23 +0900

    node, vue ssr 연결

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,50 @@
 // express server
-const express = require("express");
+const express = require("express"); // 용도 : express 사용하기 위해
+const bodyParser = require("body-parser"); // 용도 : req.body 사용하기 위해
+const cookieParser = require("cookie-parser"); // 용도 : req.cookies 사용하기 위해
+const history = require("connect-history-api-fallback");
 const app = express();
+const cors = require("cors");
+
 const port = process.env.PORT;
+// const port = 3000;
 
-// serve static files
+app.use(cors());
+app.use(history());
 app.use(express.static("dist"));
+app.use(cookieParser()); // 용도 : req.cookies 사용하기 위해
+app.use(bodyParser.json()); // 용도 : req.body 사용하기 위해
+app.use(bodyParser.urlencoded({ extended: false })); // 용도 : req.body 사용하기 위해
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post("/api/information", (req, res) => {
+  console.log("/api/information");
+
+  res.cookie("name", "express");
+
+  // send response
+  res.send("success");
+});
+
+app.get("/test", (req, res) => {
+  const { name } = req.cookies;
+
+  console.log(name);
+
+  res.cookie("name", "express");
+
+  res.send("Cookie set");
+});
 
 // start server
 app.listen(port, () => console.log(`내컴퓨터.com listening on port ${port}!`));
