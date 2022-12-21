2cd2771cf2d8476a65bd78af5630132d5b041b8e
Ryugi62 <xorjf1027@naver.com>
2022-12-21 21:05:22 +0900

    기능 : 버튼 클릭시 컴퓨터 정보가 쿠키에 있으면 사이트 이동, 없으면 프로그램 다운로드

--- frontend/src/components/CheckMyComputerButton.vue
+++ frontend/src/components/CheckMyComputerButton.vue
@@ -5,18 +5,65 @@
       class="checkButton btn btn-primary"
       @click="goListPage"
     >
-      내 PC 사양 확인하기
+      내 PC 사양 확인하기 <br />
+      (버튼을 눌러 프로그램을 설치 후 실행해주세요.)
     </button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "CheckMyComputerButton",
 
   methods: {
+    getCookie() {
+      let cookieArr = document.cookie.split("; ");
+      let cookieObj = {};
+      cookieArr.forEach((cookie) => {
+        let cookieSplit = cookie.split("=");
+        cookieObj[cookieSplit[0]] = cookieSplit[1];
+      });
+
+      return cookieObj;
+    },
+
     goListPage() {
-      this.$router.push("/gameList");
+      const {
+        bios,
+        cpu,
+        drive_capacity,
+        graphic_card,
+        mainboard_manufacturer,
+        os,
+        ram,
+      } = this.getCookie();
+
+      if (
+        bios === "" ||
+        cpu === "" ||
+        drive_capacity === "" ||
+        graphic_card === undefined ||
+        mainboard_manufacturer === "" ||
+        os === "" ||
+        ram === ""
+      ) {
+        axios({
+          url: "http://localhost:3000/api/getProgram",
+          method: "GET",
+          responseType: "blob",
+        }).then((response) => {
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", "download.zip");
+          document.body.appendChild(link);
+          link.click();
+        });
+      } else {
+        this.$router.push("/gameList");
+      }
     },
   },
 };
