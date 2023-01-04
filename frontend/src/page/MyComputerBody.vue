e369d4d89668d97107114a878934053529b55aec
Ryugi62 <xorjf1027@naver.com>
2023-01-04 19:00:43 +0900

    광고 포멧 변경

--- frontend/src/page/MyComputerBody.vue
+++ frontend/src/page/MyComputerBody.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="bodyBackground">
-    <div class="bodyLeft"></div>
+    <div class="bodyLeft">
+      <ins
+        class="kakao_ad_area"
+        style="display: none"
+        data-ad-unit="DAN-UFn2FTVzZaa5HgMa"
+        data-ad-width="160"
+        data-ad-height="600"
+      ></ins>
+    </div>
     <div class="bodyCenter">
       <router-view />
     </div>
     <div class="bodyRight">
-      <advertisement style="width: 300px; height: 250px" />
+      <div class="advertisementBox">
+        <ins
+          class="kakao_ad_area advertisement"
+          style="display: none"
+          data-ad-unit="DAN-6U9FgS8SxjRyM0qS"
+          data-ad-width="300"
+          data-ad-height="250"
+        ></ins>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Advertisement from "@/components/Advertisement.vue";
-
-export default {
-  components: {
-    Advertisement,
-  },
-};
+export default {};
 </script>
 
 <style scoped>
@@ -42,5 +52,19 @@ export default {
 .bodyRight {
   width: 300px;
   height: 100%;
+}
+
+.advertisementBox {
+  border: 1px solid black;
+  display: flex;
+  overflow: hidden;
+}
+
+.advertisement {
+  margin: auto;
+}
+
+.advertisement * {
+  object-fit: contain;
 }
 </style>
