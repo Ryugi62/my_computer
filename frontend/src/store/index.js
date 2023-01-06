4d0fa318c5d42b5a5fd21c4a7a6cc3458160b0a7
Ryugi62 <xorjf1027@naver.com>
2023-01-06 17:33:02 +0900

    admin 확인 기능 mutaion 변경

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,13 +2,12 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    isLogin: false,
+    isAdmin: false,
   },
   getters: {},
   mutations: {
-    // 로그인 상태를 변경합니다.
-    changeLoginStatus(state, payload) {
-      state.isLogin = payload;
+    setAdmin(state, payload) {
+      state.isAdmin = payload;
     },
   },
   actions: {},
