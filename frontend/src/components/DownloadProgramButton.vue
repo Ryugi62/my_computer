5e83fb27dd032e0381f1f40dcaa39a854cb60ee6
Ryugi62 <xorjf1027@naver.com>
2023-02-06 01:48:41 +0900

    실행파일의 오류로 인하여 .exe에서 .zip으로 확장자 변경

--- frontend/src/components/DownloadProgramButton.vue
+++ frontend/src/components/DownloadProgramButton.vue
@@ -20,7 +20,7 @@ export default {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", "my_computer.exe");
+        link.setAttribute("download", "my_computer.zip");
         document.body.appendChild(link);
         link.click();
       });
