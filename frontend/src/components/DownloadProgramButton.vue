e7a7faf9534387a8331181111f90ec72ece8abd9
Ryugi62 <xorjf1027@naver.com>
2023-02-11 17:41:33 +0900

    my_computer 실행파일 .zip => .exe 변경

--- frontend/src/components/DownloadProgramButton.vue
+++ frontend/src/components/DownloadProgramButton.vue
@@ -20,7 +20,7 @@ export default {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", "my_computer.zip");
+        link.setAttribute("download", "my_computer.exe");
         document.body.appendChild(link);
         link.click();
       });
